619d00c789f26290ced01cfc669ff63eb19b211a
JM <couvillion@gmail.com>
2026-09-01 23:14:14 -0400

    Switch site-wide font from Titillium Web to Inter
    Load Inter (400/500/600/700, matching the weights actually used in
    the app's CSS) and set it as the global font-family in global.css so
    it applies everywhere, plus update the old #app rule in NavBar for
    redundancy.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -116,7 +116,7 @@ export default {
 
 <style scoped>
 #app {
-  font-family: 'Titillium Web', sans-serif;;
+  font-family: 'Inter', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
